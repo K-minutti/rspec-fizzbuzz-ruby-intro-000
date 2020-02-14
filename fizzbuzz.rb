6018a9ed56f8febccc9396598d7584e94ac9c610
Kevin Minutti <minutti70@gmail.com>
2020-02-14 04:48:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,12 @@
 def fizzbuzz(i)
-   if i % 3 == 0
+  if (i % 3 == 0) && (i % 5 == 0)
+  "Fizzbuzz"
+  elsif
+   i % 3 == 0
     "Fizz"
   elsif i % 5 == 0
     "Buzz"
-  elsif (i % 3 == 0) && (i % 5 == 0)
-    "Fizzbuzz"
+  else
+
    end
  end
